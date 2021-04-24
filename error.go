d18628f34761991d00969b8441f363510e895164
csw06 <1006179273@qq.com>
2021-04-25 02:39:36 +0800

    error homework

---
+++ error.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+	"log"
+)
+
+func queryRow(db *sql.DB, name *string) error {
+	err := db.QueryRow("select name from users where id =?", 1).Scan(name)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatal(errors.Wrap(err, "QueryRow ErrNoRows"))
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	//连接数据库
+	db, err := sql.Open("mysql", "root:csw1235@/myApp")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var name string
+	err = queryRow(db, &name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("name:", name)
+}
